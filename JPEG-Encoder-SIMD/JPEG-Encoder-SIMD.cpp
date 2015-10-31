ded66230b2c6c9d1432dadd04f29bc53b659e90c
DerGary <stefangerasch1991@gmail.com>
2015-10-31 16:27:07 +0100

    BitBufferTest Picture argument 2 instead of 1

--- JPEG-Encoder-SIMD/JPEG-Encoder-SIMD.cpp
+++ JPEG-Encoder-SIMD/JPEG-Encoder-SIMD.cpp
@@ -81,10 +81,10 @@ void bitBufferTest(string filePath)
 
 int main(int argc, char* argv[])
 {
-	//benchmark("bitBufferTest", 1, [&]() {
-	//	bitBufferTest(argv[1]);
-	//});
-	//return  1;
+	benchmark("bitBufferTest", 1, [&]() {
+		bitBufferTest(argv[2]);
+	});
+	return  1;
 
 	if (argc < 3) {
 		std::cerr << "Usage: " << argv[0] << " <Source File> <Destination File>" << std::endl;
