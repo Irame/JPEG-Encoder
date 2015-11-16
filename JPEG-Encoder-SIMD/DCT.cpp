50da65329f2ff20f9a7609b676f282948012b1bc
JuliusH93 <julius.hackel@gmx.de>
2015-11-16 12:57:28 +0100

    Added seperate DCT

--- JPEG-Encoder-SIMD/DCT.cpp
+++ JPEG-Encoder-SIMD/DCT.cpp
@@ -38,3 +38,65 @@ void DCT::directDCT(const PointerMatrix& values)
 		}
 	}
 }
+
+static void seperateDCT(const PointerMatrix& values)
+{
+	// use 8x8 blocks for DCT
+	int N = 8;
+
+	// allocate memory
+	float aValues[64];
+	float resultValues[64];
+	float saveResult[64];
+
+	PointerMatrix a = PointerMatrix(aValues);
+	PointerMatrix result = PointerMatrix(resultValues);
+	PointerMatrix saveResults = PointerMatrix(saveResult);
+
+	float C = 0;
+	for (int k = 0; k < N; k++)
+	{
+		for (int n = 0; n < N; n++)
+		{
+			C = k == 0 ? 1 / sqrtf(2) : 1;
+
+			a[k][n] = C * sqrtf(2 / N)*cosf((2 * n + 1) * (k * M_PIf / 2 * N));
+		}
+	}
+
+	size_t i, j, k;
+
+	for (i = 0; i < N; i++) // A x X
+	{
+		for (j = 0; j < N; j++)
+		{
+			result[i][j] = 0.0;
+			for (k = 0; k < N; k++)
+			{
+				result[i][j] += a[i][k] * values[k][j];
+				saveResults[i][j] = result[i][j];
+			}
+		}
+	}
+
+	for (i = 0; i < N; i++) // (A x X) x aT
+	{
+		for (j = 0; j < N; j++)
+		{
+			result[i][j] = 0.0;
+			for (k = 0; k < N; k++)
+			{
+				result[i][j] += saveResults[i][k] * a.atTransposed(k, j);
+			}
+		}
+	}
+
+	// copy results to input matrix
+	for (i = 0; i < N; i++) 
+	{
+		for (j = 0; j < N; j++)
+		{
+			values[i][j] = result[i][j];
+		}
+	}
+}
--- JPEG-Encoder-SIMD/DCT.h
+++ JPEG-Encoder-SIMD/DCT.h
@@ -4,5 +4,7 @@
 namespace DCT
 {
 	static void directDCT(const PointerMatrix& values);
+
+	static void seperateDCT(const PointerMatrix& values);
 };
 
