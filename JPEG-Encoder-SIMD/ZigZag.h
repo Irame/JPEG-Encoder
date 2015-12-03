1d267e294f889323a128cd3e68b252b58939c1d8
Irame <felix_keller@gmx.de>
2015-12-03 20:11:15 +0100

    replaced calculation in ZigZag.h with precalculated array

--- JPEG-Encoder-SIMD/ZigZag.h
+++ JPEG-Encoder-SIMD/ZigZag.h
@@ -5,8 +5,10 @@
 template<typename T>
 using ZigZagReorderdArray = std::array<T, 64>;
 
+typedef std::array<size_t, 64> OffsetArray;
+
 template<typename T>
-static ZigZagReorderdArray<T> reorderByZigZag(const T* collection)
+static ZigZagReorderdArray<T> reorderByZigZagCalc(const T* collection)
 {
 	const size_t width = 8;
 	const size_t height = 8;
@@ -45,11 +47,10 @@ static ZigZagReorderdArray<T> reorderByZigZag(const T* collection)
 	return result;
 }
 
-typedef std::array<size_t, 64> OffsetArray;
 
 // Precalculated for the common 8x8 case
 static OffsetArray ZigZagOffsets8x8 {
-	 0,  1,  8, 16,  9,  2,  3, 10,
+	0,  1,  8, 16,  9,  2,  3, 10,
 	17, 24, 32, 25, 18, 11,  4,  5,
 	12, 19, 26, 33, 40, 48, 41, 34,
 	27, 20, 13,  6,  7, 14, 21, 28,
@@ -59,12 +60,25 @@ static OffsetArray ZigZagOffsets8x8 {
 	53, 60, 61, 54, 47, 55, 62, 63
 };
 
+template<typename T>
+static ZigZagReorderdArray<T> reorderByZigZag(const T* collection)
+{
+	ZigZagReorderdArray<T> result;
+	for (size_t i = 0; i < result.size(); i++)
+	{
+		result[i] = collection[ZigZagOffsets8x8[i]];
+	}
+	return result;
+}
+
 static OffsetArray createZigZagOffsetArray(size_t width)
 {
 	OffsetArray ptrArray;
-	for (size_t i = 0; i < 64; i++)
+	size_t curOffset;
+	for (size_t i = 0; i < ptrArray.size(); i++)
 	{
-		ptrArray[i] = (i % 8) + (width * (i / 8));
+		curOffset = ZigZagOffsets8x8[i];
+		ptrArray[i] = (curOffset % 8) + (width * (curOffset / 8));
 	}
-	return reorderByZigZag(ptrArray.data());
+	return ptrArray;
 }
