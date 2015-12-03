3c42f64845c90369f4633e945a79c74aa33fdd90
MBulli <markusbullmann@gmail.com>
2015-12-03 20:23:22 +0100

    Commented ZigZag.h

--- JPEG-Encoder-SIMD/ZigZag.h
+++ JPEG-Encoder-SIMD/ZigZag.h
@@ -1,14 +1,59 @@
 #pragma once
 #include <array>
 
-
 template<typename T>
 using ZigZagReorderdArray = std::array<T, 64>;
 
-typedef std::array<size_t, 64> OffsetArray;
+typedef ZigZagReorderdArray<size_t> OffsetArray;
 
+// Precalculated for the common 8x8 case
+static OffsetArray ZigZagOffsets8x8 {
+	0,  1,  8, 16,  9,  2,  3, 10,
+	17, 24, 32, 25, 18, 11,  4,  5,
+	12, 19, 26, 33, 40, 48, 41, 34,
+	27, 20, 13,  6,  7, 14, 21, 28,
+	35, 42, 49, 56, 57, 50, 43, 36,
+	29, 22, 15, 23, 30, 37, 44, 51,
+	58, 59, 52, 45, 38, 31, 39, 46,
+	53, 60, 61, 54, 47, 55, 62, 63
+};
+
+// Takes a 64 pointers and returns the same 64 pointers
+// reorders by Zig-Zag using a static offset array.
 template<typename T>
-static ZigZagReorderdArray<T> reorderByZigZagCalc(const T* collection)
+static ZigZagReorderdArray<T> reorderByZigZag(const T* collection)
+{
+	ZigZagReorderdArray<T> result;
+	for (size_t i = 0; i < result.size(); i++)
+	{
+		result[i] = collection[ZigZagOffsets8x8[i]];
+	}
+	return result;
+}
+
+// Calculates a Zig-Zag Jumptable with a given image width.
+// The resulting OffsetArray can be used to access a pointer
+// in ZigZag order. The image width is used to jump across rows.
+static OffsetArray createZigZagOffsetArray(size_t imageWidth)
+{
+	OffsetArray ptrArray;
+	size_t curOffset;
+
+	for (size_t i = 0; i < ptrArray.size(); i++)
+	{
+		curOffset = ZigZagOffsets8x8[i];
+		ptrArray[i] = (curOffset % 8) + (imageWidth * (curOffset / 8));
+	}
+
+	return ptrArray;
+}
+
+
+// Takes a 64 pointers and returns the same 64 pointers
+// reorders by Zig-Zag using loops and stuff.
+// Returns the same result like reorderByZigZag() but possibly slower.
+template<typename T>
+static ZigZagReorderdArray<T> reorderByZigZagSlow(const T* collection)
 {
 	const size_t width = 8;
 	const size_t height = 8;
@@ -45,40 +90,4 @@ static ZigZagReorderdArray<T> reorderByZigZagCalc(const T* collection)
 	}
 
 	return result;
-}
-
-
-// Precalculated for the common 8x8 case
-static OffsetArray ZigZagOffsets8x8 {
-	0,  1,  8, 16,  9,  2,  3, 10,
-	17, 24, 32, 25, 18, 11,  4,  5,
-	12, 19, 26, 33, 40, 48, 41, 34,
-	27, 20, 13,  6,  7, 14, 21, 28,
-	35, 42, 49, 56, 57, 50, 43, 36,
-	29, 22, 15, 23, 30, 37, 44, 51,
-	58, 59, 52, 45, 38, 31, 39, 46,
-	53, 60, 61, 54, 47, 55, 62, 63
-};
-
-template<typename T>
-static ZigZagReorderdArray<T> reorderByZigZag(const T* collection)
-{
-	ZigZagReorderdArray<T> result;
-	for (size_t i = 0; i < result.size(); i++)
-	{
-		result[i] = collection[ZigZagOffsets8x8[i]];
-	}
-	return result;
-}
-
-static OffsetArray createZigZagOffsetArray(size_t width)
-{
-	OffsetArray ptrArray;
-	size_t curOffset;
-	for (size_t i = 0; i < ptrArray.size(); i++)
-	{
-		curOffset = ZigZagOffsets8x8[i];
-		ptrArray[i] = (curOffset % 8) + (width * (curOffset / 8));
-	}
-	return ptrArray;
 }
